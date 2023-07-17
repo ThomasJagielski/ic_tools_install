d66cc1f5b7cc850562ea3ddf10d93931a91106ca
Thomas Jagielski <42981129+ThomasJagielski@users.noreply.github.com>
2023-07-17 14:35:43 -0400

    Update 00_install_tools.sh

--- july-2023-scripts/00_install_tools.sh
+++ july-2023-scripts/00_install_tools.sh
@@ -27,7 +27,8 @@ echo \
   
 sudo apt-get update
 sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
-sudo apt install python3.10-venv -y
+# sudo apt install python3.10-venv -y
+sudo apt install python3-venv -y
 
 # Test docker by running "sudo docker run hello-world"
 
